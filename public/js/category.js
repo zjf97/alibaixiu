a31cad6133ca0df532cb38350c623ec5fcfbaca1
ZhangJianfei <kingxff@163.com>
2019-12-13 10:50:59 +0800

    实现分类单个与多个删除

--- public/js/category.js
+++ public/js/category.js
@@ -1,11 +1,12 @@
-$('#addClass').on('submit', function () {
+$('#className').on('submit', '#addClass', function () {
     var formData = $(this).serialize()
     $.ajax({
         type: "post",
         url: "/categories",
         data: formData,
         success: function (response) {
-            console.log(response);
+            // console.log(response);
+            location.reload()
         }
     });
     return false
@@ -16,10 +17,87 @@ $.ajax({
     url: "/categories",
     success: function (response) {
         // console.log(response);
-        
-        var html = template('listTpl', {data: response})
+
+        var html = template('listTpl', { data: response })
         // console.log(html);
-        
+
         $('#classList').html(html)
     }
 });
+
+
+$('#classList').on('click', '.edit', function () {
+    var id = $(this).attr('data-id')
+    $.ajax({
+        type: "get",
+        url: "/categories/" + id,
+        success: function (response) {
+            var html = template('editTpl', response)
+            $('#className').html(html)
+        }
+    });
+})
+
+$('#className').on('submit', '#editClass', function () {
+    var formData = $(this).serialize()
+    var id = $(this).attr('data-id')
+    $.ajax({
+        type: "put",
+        url: "/categories/" + id,
+        data: formData,
+        success: function (response) {
+            location.reload()
+        }
+    });
+    return false
+})
+
+$('#classList').on('click', '.delete', function () {
+    var id = $(this).attr('data-id')
+    $.ajax({
+        type: "delete",
+        url: "/categories/" + id,
+        success: function (response) {
+            location.reload()
+        }
+    });
+})
+
+$('#cheAll').on('change', function () {
+    var status = $(this).prop('checked')
+    $('#classList').find('input').prop('checked', status)
+    $('#deleteBtn').css({ 'display': 'block' })
+    if ($('#classList').find('input').filter(':checked').length == 0) {
+        $('#deleteBtn').css({ 'display': 'none' })
+    }
+})
+
+$('#classList').on('change', 'input', function () {
+    $('#deleteBtn').css({ 'display': 'block' })
+    if ($('#classList').find('input').filter(':checked').length == $('#classList').find('input').length) {
+        $('#cheAll').prop('checked', true)
+    } else {
+        $('#cheAll').prop('checked', false)
+    }
+
+    if ($('#classList').find('input').filter(':checked').length == 0) {
+        $('#deleteBtn').css({ 'display': 'none' })
+    }
+})
+
+$('#deletes').on('click', function () { 
+    var arr = []
+    $.each($('#classList').find('input').filter(':checked'), function (index, value) { 
+         arr.push($(value).attr('data-id'))
+    });
+    var id = arr.join('-')
+    console.log(id);
+    
+    $.ajax({
+        type: "delete",
+        url: "/categories/" + id,
+        success: function (response) {
+            location.reload()
+        }
+    });
+})
